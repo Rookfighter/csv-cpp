cd09ccf121a0ae4a495da199a2b8d72ca345ed11
Fabian Meyer <user8324@posteo.net>
2018-07-21 11:06:40 +0200

    Fix build system

---
+++ test/main.cpp
@@ -0,0 +1,10 @@
+/*
+ * main.cpp
+ *
+ * Created on: 01 Feb 2018
+ *     Author: Fabian Meyer
+ *    License: MIT
+ */
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
--- test/test_csvfile.cpp
+++ test/test_csvfile.cpp
@@ -1,16 +1,13 @@
 /*
- * TestCSV.cpp
+ * tes_csvfile.cpp
  *
  * Created on: 01 Feb 2018
  *     Author: Fabian Meyer
  *    License: MIT
  */
 
-#define CATCH_CONFIG_MAIN
-
 #include <catch.hpp>
-#include <iostream>
-#include "CSV.hpp"
+#include "csvcpp.h"
 
 TEST_CASE("parse csv file", "CsvFile")
 {
@@ -20,15 +17,15 @@ TEST_CASE("parse csv file", "CsvFile")
     REQUIRE(csvf.size() == 2);
 
     REQUIRE(csvf[0].size() == 3);
-    REQUIRE(csvf[0][0].asString() == "foo");
-    REQUIRE(csvf[0][1].asBool() == true);
-    REQUIRE(csvf[0][2].asDouble() == 1.0);
+    REQUIRE(csvf[0][0].as<std::string>() == "foo");
+    REQUIRE(csvf[0][1].as<bool>() == true);
+    REQUIRE(csvf[0][2].as<double>() == 1.0);
 
     REQUIRE(csvf[1].size() == 4);
-    REQUIRE(csvf[1][0].asString() == "bar");
-    REQUIRE(csvf[1][1].asBool() == false);
-    REQUIRE(csvf[1][2].asInt() == 200);
-    REQUIRE(csvf[1][3].asString() == "Hello, World!");
+    REQUIRE(csvf[1][0].as<std::string>() == "bar");
+    REQUIRE(csvf[1][1].as<bool>() == false);
+    REQUIRE(csvf[1][2].as<int>() == 200);
+    REQUIRE(csvf[1][3].as<std::string>() == "Hello, World!");
 }
 
 TEST_CASE("parse row", "CsvFile")
@@ -41,10 +38,10 @@ TEST_CASE("parse row", "CsvFile")
 
     REQUIRE(row.size() == 4);
 
-    REQUIRE(row[0].asString() == "foo");
-    REQUIRE(row[1].asBool() == true);
-    REQUIRE(row[2].asDouble() == 1.0);
-    REQUIRE(row[3].asInt() == 10);
+    REQUIRE(row[0].as<std::string>() == "foo");
+    REQUIRE(row[1].as<bool>() == true);
+    REQUIRE(row[2].as<double>() == 1.0);
+    REQUIRE(row[3].as<int>() == 10);
 }
 
 TEST_CASE("parse row escaped", "CsvFile")
@@ -57,11 +54,11 @@ TEST_CASE("parse row escaped", "CsvFile")
 
     REQUIRE(row.size() == 5);
 
-    REQUIRE(row[0].asString() == "foo");
-    REQUIRE(row[1].asBool() == true);
-    REQUIRE(row[2].asDouble() == 1.0);
-    REQUIRE(row[3].asInt() == 10);
-    REQUIRE(row[4].asString() == "Hello, World\t!");
+    REQUIRE(row[0].as<std::string>() == "foo");
+    REQUIRE(row[1].as<bool>() == true);
+    REQUIRE(row[2].as<double>() == 1.0);
+    REQUIRE(row[3].as<int>() == 10);
+    REQUIRE(row[4].as<std::string>() == "Hello, World\t!");
 }
 
 TEST_CASE("parse row single char values", "CsvFile")
@@ -74,9 +71,9 @@ TEST_CASE("parse row single char values", "CsvFile")
 
     REQUIRE(row.size() == 3);
 
-    REQUIRE(row[0].asInt() == 1);
-    REQUIRE(row[1].asInt() == 2);
-    REQUIRE(row[2].asInt() == 3);
+    REQUIRE(row[0].as<int>() == 1);
+    REQUIRE(row[1].as<int>() == 2);
+    REQUIRE(row[2].as<int>() == 3);
 }
 
 TEST_CASE("add values", "CsvFile")
@@ -89,15 +86,15 @@ TEST_CASE("add values", "CsvFile")
     REQUIRE(csvf.size() == 2);
 
     REQUIRE(csvf[0].size() == 3);
-    REQUIRE(csvf[0][0].asString() == "foo");
-    REQUIRE(csvf[0][1].asBool() == true);
-    REQUIRE(csvf[0][2].asDouble() == 1.0);
+    REQUIRE(csvf[0][0].as<std::string>() == "foo");
+    REQUIRE(csvf[0][1].as<bool>() == true);
+    REQUIRE(csvf[0][2].as<double>() == 1.0);
 
     REQUIRE(csvf[1].size() == 4);
     //REQUIRE(csvf[1][0].asString() == "bar");
-    REQUIRE(csvf[1][1].asBool() == false);
-    REQUIRE(csvf[1][2].asInt() == 200);
-    REQUIRE(csvf[1][3].asFloat() == 2.1f);
+    REQUIRE(csvf[1][1].as<bool>() == false);
+    REQUIRE(csvf[1][2].as<int>() == 200);
+    REQUIRE(csvf[1][3].as<float>() == 2.1f);
 }
 
 TEST_CASE("encode without escape", "CsvFile")
